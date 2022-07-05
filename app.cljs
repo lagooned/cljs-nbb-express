8553bc14afd20b02aa38f5d8b527efcc99cf37e2
lagooned <jared.lite@gmail.com>
2022-07-05 15:21:20 -0500

    initial commit

---
+++ app.cljs
@@ -0,0 +1,15 @@
+(ns cljs-nbb-express
+  (:require ["express$default" :as express]))
+
+(def app (express))
+(def port 8092)
+
+(.get
+ app "/"
+ (fn foo [_req res]
+   (.send res "Hello World!")))
+
+(.listen
+ app
+ port
+ (fn [] (println "Example app listening on port" port)))
